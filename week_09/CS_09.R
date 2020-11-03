b80ae1cb6c87c1e41aea31fc3f6fabc9e3f7caeb
smnotaro <smnotaro@buffalo.edu>
2020-11-02 22:00:53 -0500

    First commit

--- week_09/CS_09.R
+++ week_09/CS_09.R
@@ -1,13 +1,3 @@
----
-title: "Case Study 09"
-author: Sandra Notaro
-date: November 1, 2020
-output: github_document
----
-
-# Preparing the data
-
-```{r prep, eval=T, echo=F, message=F}
 library(sf)
 library(tidyverse)
 library(ggmap)
@@ -16,28 +6,23 @@ library(spData)
 data(world)
 data(us_states)
 library(dplyr)
-```
 
-``````{r prep2, eval=T, echo=T, warning=F, message=F}
 dataurl="https://www.ncei.noaa.gov/data/international-best-track-archive-for-climate-stewardship-ibtracs/v04r00/access/shapefile/IBTrACS.NA.list.v04r00.points.zip"
 tdir=tempdir()
 download.file(dataurl,destfile=file.path(tdir,"temp.zip"))
 unzip(file.path(tdir,"temp.zip"),exdir = tdir)
 list.files(tdir)
 storm_data <- read_sf(list.files(tdir,pattern=".shp",full.names = T))
-```
 
-# Filtering for years 1950s to present and identifying the bounding box of the storm data
-```{r filter, echo=T, eval =T}
+# Filtering for years 1950s to present
 filtered_1950_present <- storm_data %>%
   filter(SEASON >= 1950) %>%
   mutate_if(is.numeric, function(x) ifelse(x==-999.0,NA,x)) %>%
   mutate(decade=(floor(year/10)*10))
+  
 region <- st_bbox(filtered_1950_present)
-```
 
 # Plotting maps
-``` {r prepplot, echo=F, eval=T, message=F}
 storm_plots <- ggplot(world) + geom_sf() + facet_wrap(~decade) +
   stat_bin2d(data = filtered_1950_present, aes(y = st_coordinates(filtered_1950_present)[,2], 
                               x = st_coordinates(filtered_1950_present)[,1]), bins = 100) +
@@ -45,37 +30,25 @@ storm_plots <- ggplot(world) + geom_sf() + facet_wrap(~decade) +
                        breaks = c(1,10,100,1000)) + coord_sf(ylim=region[c(2,4)], 
                                                              xlim=region[c(1,3)]) +
   theme(axis.title.x = element_blank(), axis.title.y = element_blank())
-```
-
-```{r plot, echo = F, eval = T}
 print(storm_plots)
-```
 
-# Reprojecting `us_states` to the reference system of `filtered_1950_present` and renaming `NAME` column to `state`
-
-```{r edits, echo=TRUE, eval =T}
+# Reprojecting us_states to the reference system of filtered_1950_present and
+#renaming NAME column to state
 state <- us_states %>%
   st_transform(crs = st_crs(filtered_1950_present)) %>%
   select(state = NAME)
-```
 
-# Spatial join between storm database and `state` object
-```{r join, echo=T, eval =T, warning=F, message=F}
+# Spatial join between storm database and states object
 storm_states <- st_join(filtered_1950_present, state, join = st_intersects,left = F)
-```
 
 # Making the table
-```{r table prep, echo = F, eval=T}
 table <- storm_states %>%
   group_by(state) %>%
   summarize(filtered_1950_present =length(unique(NAME))) %>%
   arrange(desc(filtered_1950_present)) %>%
   slice(1:5) %>%
   st_set_geometry(NULL)
-```
-
-``` {r table, echo = F, eval = T, warning = F, messgae = F}
 knitr::kable(table, "simple")
-```
 
-Learned how to get rid of geometry column from https://r-spatial.github.io/sf/reference/st_geometry.html
+
+
---
+++ week_09/case_study_09.md
@@ -0,0 +1,65 @@
+Case Study 09
+================
+Sandra Notaro
+November 1, 2020
+
+# Preparing the data
+
+``` r
+dataurl="https://www.ncei.noaa.gov/data/international-best-track-archive-for-climate-stewardship-ibtracs/v04r00/access/shapefile/IBTrACS.NA.list.v04r00.points.zip"
+tdir=tempdir()
+download.file(dataurl,destfile=file.path(tdir,"temp.zip"))
+unzip(file.path(tdir,"temp.zip"),exdir = tdir)
+list.files(tdir)
+```
+
+    ## [1] "IBTrACS.NA.list.v04r00.points.dbf" "IBTrACS.NA.list.v04r00.points.prj"
+    ## [3] "IBTrACS.NA.list.v04r00.points.shp" "IBTrACS.NA.list.v04r00.points.shx"
+    ## [5] "temp.zip"
+
+``` r
+storm_data <- read_sf(list.files(tdir,pattern=".shp",full.names = T))
+```
+
+# Filtering for years 1950s to present and identifying the bounding box of the storm data
+
+``` r
+filtered_1950_present <- storm_data %>%
+  filter(SEASON >= 1950) %>%
+  mutate_if(is.numeric, function(x) ifelse(x==-999.0,NA,x)) %>%
+  mutate(decade=(floor(year/10)*10))
+region <- st_bbox(filtered_1950_present)
+```
+
+# Plotting maps
+
+![](case_study_09_files/figure-gfm/plot-1.png)<!-- -->
+
+# Reprojecting `us_states` to the reference system of `filtered_1950_present` and renaming `NAME` column to `state`
+
+``` r
+state <- us_states %>%
+  st_transform(crs = st_crs(filtered_1950_present)) %>%
+  select(state = NAME)
+```
+
+# Spatial join between storm database and `state` object
+
+``` r
+storm_states <- st_join(filtered_1950_present, state, join = st_intersects,left = F)
+```
+
+# Making the table
+
+    ## `summarise()` ungrouping output (override with `.groups` argument)
+
+| state          | filtered\_1950\_present |
+| :------------- | ----------------------: |
+| Florida        |                      82 |
+| North Carolina |                      62 |
+| Georgia        |                      57 |
+| Texas          |                      53 |
+| Louisiana      |                      49 |
+
+Learned how to get rid of geometry column from
+<https://r-spatial.github.io/sf/reference/st_geometry.html>
